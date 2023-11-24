c981550fba414e6f2309a053a93820102b1a0a7a
Sizan458 <www.shijan23@gmail.com>
2023-11-24 09:39:23 +0600

    make route & add some packages

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
-
+import { RouterProvider } from 'react-router-dom'
+import Router from './component/Router/Router'
+import {  HelmetProvider } from 'react-helmet-async';
+import {
+  
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+// Create a client
+const queryClient = new QueryClient()
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+ <QueryClientProvider client={queryClient}>
+ <HelmetProvider>
+  <RouterProvider router={Router}/>
+  </HelmetProvider>
+ </QueryClientProvider>
   </React.StrictMode>,
 )
