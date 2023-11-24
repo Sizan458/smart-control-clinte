01fc06324e9d613bd5999ccf77979dfd758edde6
Sizan458 <www.shijan23@gmail.com>
2023-11-24 11:41:58 +0600

    add banner & style it

--- src/component/HomePage/Home/Home.jsx
+++ src/component/HomePage/Home/Home.jsx
@@ -1,7 +1,15 @@
+import { Helmet } from "react-helmet-async";
+import Slider from "../Slider/Slider";
+
 const Home = () => {
     return (
         <div>
-            <h1>juuuu</h1>
+            <Helmet>
+          <title>  Smart Build Control || Home</title>
+            </Helmet>
+          <div>
+            <Slider/>
+          </div>
         </div>
     );
 };
